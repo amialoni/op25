727a1372fecae0fae37911c44d0dbd543f672ad0
Ami Aloni <amialoni@gmail.com>
2025-08-18 19:26:39 +0300

    update url

--- op25/gr-op25_repeater/apps/run.sh
+++ op25/gr-op25_repeater/apps/run.sh
@@ -5,25 +5,27 @@ cd ~/op25/op25/gr-op25_repeater/apps/
 pkill -f rx.py || true
 pkill -f ffmpeg || true
 pkill -f http.server || true
-sleep 1
+sleep 2
 rtl_test -t
 # Stop flag
 if [[ "$1" == "-s" ]]; then
-    echo "Stopped OP25 and FFmpeg."
+    echo "Stopped OP25, FFmpeg and HTTP server."
     exit 0
 fi
 
 RTL_DEV="rtl=1"
 # -o 500
 # Start OP25 receiver
-nohup ./rx.py --nocrypt \
+
+# nohup 
+./rx.py --nocrypt \
     --args "$RTL_DEV" \
-    --gains "lna:30" \
+    --gains "lna:32" \
     -S 1000000 \
-    -q 18  \
+    -q 19  \
     -d 0 -v 1 -2 -f 867.475e6 \
     -X -V -w \
-    -l http:0.0.0.0:8080 -P fft &
+    -l http:0.0.0.0:8080 &
 
 sleep 5
 
@@ -35,8 +37,16 @@ sudo python3 -m http.server 81 &
 #  -f hls -hls_time 5 -hls_list_size 12 -hls_flags delete_segments \
 #  /srv/www/myradio.tovmeod.com/op25/op25.m3u8 &
 
-nohup ffmpeg -re -f s16le -ar 8000 -ac 1 -i udp://127.0.0.1:23456 \
+# nohup ffmpeg -re -f s16le -ar 8000 -ac 1 -i udp://127.0.0.1:23456 \
+# -c:a aac -b:a 64k \
+# -f hls -hls_time 5 -hls_list_size 52 -hls_flags delete_segments+append_list \
+#  html/op25.m3u8 &
+
+ ffmpeg -re -f s16le -ar 8000 -ac 1 -i udp://127.0.0.1:23456 \
+-f lavfi -t 3600 -i aevalsrc=0::duration=3600:sample_rate=8000 \
+-filter_complex "[0:a][1:a]amix=inputs=2:dropout_transition=9999" \
 -c:a aac -b:a 64k \
--f hls -hls_time 5 -hls_list_size 52 -hls_flags delete_segments+append_list \
- html/op25.m3u8 &
+-f hls -hls_time 5 -hls_list_size 0 -hls_flags append_list+omit_endlist \
+html/op25.m3u8 &
+
 
